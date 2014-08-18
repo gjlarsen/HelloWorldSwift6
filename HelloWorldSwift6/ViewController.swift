0e56a1051bc61b494ee7f25fa98c17c5bebec355
Grant Larsen <grant@glarsen.com>
2014-08-18 05:32:23 -0600

    Initial Commit

---
+++ HelloWorldSwift6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldSwift6
+//
+//  Created by Grant Larsen on 8/18/14.
+//  Copyright (c) 2014 innov8s. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
